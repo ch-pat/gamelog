0662c01f43678a457088f7b44ea013021d391009
Patryk <patryk.choma@gmail.com>
2019-10-30 22:21:10 +0100

    moved files

--- application/main.py
+++ application/main.py
@@ -1,8 +1,11 @@
 from bl_parser import bl_parser
 from gamelist import gamelist
 
-if __name__ == "__main__":
+def main():
     parser = bl_parser()
     games, plats = parser.parse_to_gamelist("backlog_paste.txt")
     gl = gamelist(games, plats)
     gl.export_to_csv("backlog.csv")
+
+if __name__ == "__main__":
+    main()
