d15ee2d13faf6e52b3d70990555d0606f0fb4552
Patryk <patryk.choma@gmail.com>
2020-02-29 13:43:28 +0100

    rearranged main

--- application/main.py
+++ application/main.py
@@ -2,10 +2,12 @@ from gamelist import gamelist
 
 
 def main():
+    gl = gamelist()
+    gl.import_from_backloggery("backlog_paste.txt")
     # TODO: implement ui and main logic
     return
 
 
 if __name__ == "__main__":
-    gl = gamelist()
-    gl.import_from_backloggery("backlog_paste.txt")
+    from gamelist import gamelist
+    from game import game
